35e8c4f9ca491a5d7c091e85bc4e64a74c0c6999
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-09-07 12:28:30 +0200

    Include medium-voltage calculation in new projects

--- src/data/emptyProject.js
+++ src/data/emptyProject.js
@@ -1,10 +1,14 @@
 import { createProject } from '../models/Project'
-import { createLowVoltageCableCalculation } from '../models/Calculation'
+import {
+  createLowVoltageCableCalculation,
+  createMediumVoltageCableCalculation,
+} from '../models/Calculation'
 
 export function createEmptyProject() {
   return createProject({
     calculations: [
       createLowVoltageCableCalculation(),
+      createMediumVoltageCableCalculation(),
     ],
   })
 }
